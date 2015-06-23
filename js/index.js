12961d32a172aac617bc94456113f776c91e667a
sujoyu <sujoyu.motoki@gmail.com>
2015-06-23 17:16:50 +0900

    ignore SharedWorker becourse jquery_plantuml does not work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function() {
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/diagram"); 
+  editor.getSession().setMode("ace/mode/diagram");
 
   var $form = $("#previewForm");
   
@@ -14,13 +14,48 @@ $(function() {
     readOnly: false
   });
 
+  plantumlPreviewer.finalize();
 
   $form.submit(function() {
-    $("#canvas").attr("uml", editor.getValue());
-    $("img").each(function() {
-      $(this).attr("src", null);
+    $("#canvas").attr({
+      uml: editor.getValue(),
+      src: null
     });
+
+    plantumlPreviewer.initialize();
     plantuml_runonce();
+    plantumlPreviewer.finalize();
+    
     return false;
   });
 });
+
+var plantumlPreviewer = {
+
+  SharedWorker: null,
+  Worker: null,
+
+  initialize: function() {
+    if (window.SharedWorker) {
+      this.SharedWorker = window.SharedWorker;
+      window.SharedWorker = undefined;
+    }
+
+    if (window.Worker) {
+      this.Worker = window.Worker;
+      window.Worker = undefined;
+    }
+  },
+
+  finalize: function() {
+    if (this.SharedWorker) {
+      window.SharedWorker = this.SharedWorker;
+    }
+
+    if (this.Worker) {
+      window.Worker = this.Worker;
+    }
+  }
+}
+
+plantumlPreviewer.initialize();
