04e4d080b3d4c53aa5c55d83e82303843938a5e0
sujoyu <sujoyu.motoki@gmail.com>
2015-06-24 21:53:37 +0900

    impelement save/load and keybinding mode

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 $(function() {
+  var $mode = $("#mode");
+  
+  var ls = window.localStorage;
+
+  var uml = ls && ls.getItem("uml");
+  if (uml) {
+    $("#editor").html(uml);
+  }
+  
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/diagram");
 
   var $form = $("#previewForm");
-  
-  editor.commands.addCommand({
-    name: 'submit',
-    bindKey: {win: 'Shift-Enter',  mac: 'Shift-Enter'},
-    exec: function(editor) {
-      $form.submit();
-    },
-    readOnly: false
-  });
 
   plantumlPreviewer.finalize();
 
   $form.submit(function() {
+    var uml = editor.getValue();
+
+    ls && ls.setItem("uml", uml);
+    
     $("#canvas").attr({
-      uml: editor.getValue(),
+      uml: uml,
       src: null
     });
 
@@ -28,5 +32,34 @@ $(function() {
     
     return false;
   });
+
+  $mode.change(function() {
+    var mode = $(this).val();
+    
+    addSubmitKey();
+
+    ls && ls.setItem("mode", mode);
+    editor.setKeyboardHandler(mode);
+  });
+
+  var mode = ls && ls.getItem("mode");
+  if (mode) {
+    $mode.val(mode);
+    $mode.change();
+  }
+
+  function addSubmitKey() {
+    editor.commands.addCommand({
+    name: 'submit',
+    bindKey: {win: 'Shift-Enter',  mac: 'Shift-Enter'},
+    exec: function(editor) {
+      $form.submit();
+    },
+    readOnly: false
+    });
+  }
+
+  addSubmitKey();
+  
 });
 
